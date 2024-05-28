0f01ba6b5d200dae23e23f823b3287a82b0fd31f
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-05-27 21:25:14 -0300

    style: Configura styled guide de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2D5FFF",
+        red: "#BF3B44",
+        BaseGray100: "#F9F0F0",
+        BaseGray300: "#DEDEDE",
+        BaseGray700: "#4E4E4E",
+        BaseGray1000: "#000000",
+        white: "#ffffff",
+      }
+    },
   },
   plugins: [],
 }
